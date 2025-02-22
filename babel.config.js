697a47c53712043dd36a32f843714749a37537c5
wadekar9 <npwadekar7@gmail.com>
2025-02-22 16:35:45 +0530

    chore : upgrade react native to 0.75.3

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
 const { putResolve } = require("redux-saga/effects");
 
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: ['module:@react-native/babel-preset'],
   plugins: ['react-native-reanimated/plugin']
 };
