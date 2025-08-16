fab6de1dfee3aee7668b57737aee3ff3f74d90b6
wadekar9 <npwadekar7@gmail.com>
2025-08-16 19:00:16 +0530

    chore : updrage project react native verion to 0.78.3

--- index.js
+++ index.js
@@ -2,11 +2,12 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
-import {Provider} from 'react-redux';
-import {store} from './src/redux/store/index.ts';
+import { name as appName } from './app.json';
+import { Provider } from 'react-redux';
+import { store } from './src/redux/store/index.ts';
+import AnimationApp from './src/ritik-prasad-animation-video/index.tsx';
 
 const ReduxApp = () => {
   return (
@@ -16,4 +17,4 @@ const ReduxApp = () => {
   );
 };
 
-AppRegistry.registerComponent(appName, () => ReduxApp);
+AppRegistry.registerComponent(appName, () => AnimationApp);
